578ddfacbc279e134664a97e78fac2658b6ef9ae
Lucas Pütz <vE5li@tuta.io>
2020-02-16 22:31:03 +0100

    added install and uninstall scripts

---
+++ utility/install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# make sure that seamonkey is not already installed
+
+# install rust if its missing
+
+# create directory
+script_path="$( cd "$(dirname "$0")" ; pwd -P )"
+sudo mkdir /usr/share/seamonkey
+sudo chown "$USER" /usr/share/seamonkey
+cd /usr/share/seamonkey
+
+# install igt5
+git clone https://github.com/ve5li/igt5
+cd igt5
+cargo build
+sudo cp target/debug/igt5 /usr/bin/seamonkey
+cd ..
+
+# install qtc8
+git clone https://github.com/ve5li/qtc8
+
+# install h0vs
+git clone https://github.com/ve5li/h0vs
+sudo cp "$script_path/cipher" /usr/bin/cipher
+sudo chmod +x /usr/bin/cipher
+
+# install ktl9
+git clone https://github.com/ve5li/ktl9
+sudo cp "$script_path/opal" /usr/bin/opal
+sudo chmod +x /usr/bin/opal
+
+# install jts3
+git clone https://github.com/ve5li/jts3
+sudo cp "$script_path/entleman" /usr/bin/entleman
+sudo chmod +x /usr/bin/entleman
---
+++ utility/uninstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# delete seamonkey
+sudo rm -rf /usr/share/seamonkey
+
+# remove start scripts
+sudo rm /usr/bin/seamonkey
+sudo rm /usr/bin/cipher
+sudo rm /usr/bin/opal
+sudo rm /usr/bin/entleman
