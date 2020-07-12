9ea0ca5eae337a40ebd455db91517340fab5e344
Lucas Pütz <vE5li@tuta.io>
2020-07-13 00:36:12 +0200

    updated file extentions and todo

--- example/main.asm
+++ example/main.asm
@@ -1,10 +1,14 @@
+@ TODO: implement floats
+@ TODO: calculate memory size based on avalible space
+
 core {
     module instruction
     module selector
     module adder
+    module comparitor
     register data[2]
-    register operant[2]
     @register memory[2]
+    register operant[2]
 }
 
 define {
@@ -16,11 +20,15 @@ define {
 memory main {
     command_display:
     string #command_prefix
+    memory_start:
 }
 
 code fibonacci {
     start:
     [ 1 -> data:slot0 | 1 -> data:slot1 ]
+
+@    [ $memory_start -> selector:address ]
+@    [ 255 -> memory:slot0 | '6' -> memory:slot1 ]
 
     .loop:
     [ data:slot0 -> adder0:in0 | data:slot1 -> adder0:in1 ]
@@ -28,10 +36,22 @@ code fibonacci {
     [ .loop -> instruction:jump | #true -> instruction:condition ]
 }
 
-memory_map {
+code {
+    send_string:
+@    [ memory:slot0 -> comparitor0:in0 | 0 -> data:slot0 | 1 -> adder0:in0 ]
+
+    .loop:
+    [ selector:address -> adder0:in1 ]
+    [ adder0:out -> selector:address ]
+
+    [ data:slot0 -> comparitor0:in1 | data:slot0 -> adder0:in1 ]
+    [ .loop -> instruction:jump | comparitor0:equal -> instruction:condition | adder0:out -> data:slot0 ]
+}
+
+map memory {
     #memory_offset -> main -> #default
 }
 
-code_map {
+map code {
     #code_offset -> fibonacci -> #default
 }
