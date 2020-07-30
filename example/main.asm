fdf58635ea59a3eb302110104a34c352f3f56ad9
Lucas Pütz <vE5li@tuta.io>
2020-07-30 20:41:37 +0200

    removed todo from code files

--- example/main.asm
+++ example/main.asm
@@ -1,13 +1,10 @@
-@ TODO: implement floats
-@ TODO: calculate memory size based on avalible space
-
 core {
     module instruction
     module selector
     module adder
     module comparitor
     register data[2]
-    @register memory[2]
+    register memory[2]
     register operant[2]
 }
 
@@ -27,8 +24,8 @@ code fibonacci {
     start:
     [ 1 -> data:slot0 | 1 -> data:slot1 ]
 
-@    [ $memory_start -> selector:address ]
-@    [ 255 -> memory:slot0 | '6' -> memory:slot1 ]
+    [ $memory_start -> selector:address ]
+    [ 255 -> memory:slot0 | '6' -> memory:slot1 ]
 
     .loop:
     [ data:slot0 -> adder0:in0 | data:slot1 -> adder0:in1 ]
@@ -38,7 +35,7 @@ code fibonacci {
 
 code {
     send_string:
-@    [ memory:slot0 -> comparitor0:in0 | 0 -> data:slot0 | 1 -> adder0:in0 ]
+    [ memory:slot0 -> comparitor0:in0 | 0 -> data:slot0 | 1 -> adder0:in0 ]
 
     .loop:
     [ selector:address -> adder0:in1 ]
