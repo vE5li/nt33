fa2a26117684e5033db28e13184776780b13e6d4
Lucas Pütz <vE5li@tuta.io>
2020-08-27 16:44:56 +0200

    finalized project names

--- utility/install.sh
+++ utility/install.sh
@@ -1,71 +1,72 @@
 #!/bin/bash
 
-# create seamonkey directory if its missing
-if [ ! -d /usr/share/seamonkey ]; then
-    sudo mkdir /usr/share/seamonkey &&
-    sudo chown "$USER" /usr/share/seamonkey
+# create kami directory if its missing
+if [ ! -d /usr/share/kami ]; then
+    sudo mkdir /usr/share/kami &&
+    sudo chown "$USER" /usr/share/kami
 fi
 
 # get path to untility folder
 script_path="$( cd "$(dirname "$0")" ; pwd -P )"
-cd /usr/share/seamonkey &&
+cd /usr/share/kami &&
+echo -e "\n"
 
-# install dst6
-if [ ! -d dst6 ]; then
-    git clone https://github.com/ve5li/dst6 &&
-    echo -e "\ndst6 installed successfully\n"
+# install seamonkey
+if [ ! -d seamonkey ]; then
+    git clone https://github.com/ve5li/seamonkey &&
+    echo -e "\nseamonkey installed successfully\n"
 fi
 
-# install igt5
-if [ ! -d igt5 ]; then
-    git clone https://github.com/ve5li/igt5 &&
-    echo -e "\nigt5 installed successfully\n"
+# install seashell
+if [ ! -d seashell ]; then
+    git clone https://github.com/ve5li/seashell &&
+    echo -e "\nseashell installed successfully\n"
 fi
 
-# install qtc8
-if [ ! -d qtc8 ]; then
-    git clone https://github.com/ve5li/qtc8 &&
-    echo -e "\nqtc8 installed successfully\n"
+# install shego
+if [ ! -d shego ]; then
+    git clone https://github.com/ve5li/shego &&
+    echo -e "\nshego installed successfully\n"
 fi
 
-# install h0vs
-if [ ! -d h0vs ]; then
-    git clone https://github.com/ve5li/h0vs &&
+# install cipher
+if [ ! -d cipher ]; then
+    git clone https://github.com/ve5li/cipher &&
     sudo cp "$script_path/cipher" /usr/bin/cipher &&
     sudo chmod +x /usr/bin/cipher &&
-    echo -e "\nh0vs installed successfully\n"
+    echo -e "\ncipher installed successfully\n"
 fi
 
-# install ktl9
-if [ ! -d ktl9 ]; then
-    git clone https://github.com/ve5li/ktl9 &&
+# install doofenshmirtz
+if [ ! -d doofenshmirtz ]; then
+    git clone https://github.com/ve5li/doofenshmirtz &&
     sudo cp "$script_path/doofenshmirtz" /usr/bin/doofenshmirtz &&
     sudo chmod +x /usr/bin/doofenshmirtz &&
-    echo -e "\nktl9 installed successfully\n"
+    echo -e "\ndoofenshmirtz installed successfully\n"
 fi
 
-# install jts3
-if [ ! -d jts3 ]; then
-    git clone https://github.com/ve5li/jts3 &&
+# install entleman
+if [ ! -d entleman ]; then
+    git clone https://github.com/ve5li/entleman &&
     sudo cp "$script_path/entleman" /usr/bin/entleman &&
     sudo chmod +x /usr/bin/entleman &&
-    echo -e "\njts3 installed successfully\n"
+    echo -e "\nentleman installed successfully\n"
 fi
 
 # install rust if its missing
 if [ ! -d "$HOME/.cargo" ]; then
     echo -e "\ncargo was not found on your system. running rustup..."
-    echo -e "please remember that seamonkey requires the nightly toolchain to compile\n"
+    echo -e "please remember that seashell requires the nightly toolchain to compile\n"
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh &&
     source "$HOME/.cargo/env" &&
     echo -e "\ncargo installed successfully\n"
 fi
 
-# compile igt5
-if [ ! -f /usr/bin/seamonkey ]; then
-    cd igt5 && cargo build &&
-    sudo cp target/debug/igt5 /usr/bin/seamonkey
-    echo -e "\nigt5 compiled successfully\n"
+# compile seashell
+if [ ! -f /usr/bin/seashell ]; then
+    cd seashell && cargo build &&
+    sudo cp target/debug/seashell /usr/bin/seashell
+    echo -e "\nseashell compiled successfully\n"
 fi
 
 # return to original working directory
--- utility/uninstall.sh
+++ utility/uninstall.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# delete seamonkey
-sudo rm -rf /usr/share/seamonkey
+# delete kami
+sudo rm -rf /usr/share/kami
 
 # remove start scripts
-sudo rm /usr/bin/seamonkey
+sudo rm /usr/bin/seashell
 sudo rm /usr/bin/cipher
 sudo rm /usr/bin/doofenshmirtz
 sudo rm /usr/bin/entleman
