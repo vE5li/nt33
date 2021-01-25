c6b83185c9fc7cfcbb20ab46d58c135585217983
Lucas Pütz <ve5lituta.io>
2021-01-25 09:25:27 +0100

    added liquify and updated paths

--- utility/install.sh
+++ utility/install.sh
@@ -1,37 +1,42 @@
 #!/bin/bash
-
-# create tortoise directory if its missing
-if [ ! -d /usr/share/tortoise ]; then
-    sudo mkdir /usr/share/tortoise &&
-    sudo chown "$USER" /usr/share/tortoise
-fi
-
-# get path to untility folder
 script_path="$( cd "$(dirname "$0")" ; pwd -P )"
-cd /usr/share/tortoise &&
+cd /usr/share &&
 echo -e "\n"
 
 # install seamonkey
-if [ ! -d seamonkey ]; then
-    git clone https://github.com/ve5li/seamonkey &&
+#if [ ! -d seamonkey ]; then
+    sudo git clone https://github.com/ve5li/seamonkey &&
+    sudo chown -R "$USER" seamonkey/
     echo -e "\nseamonkey installed successfully\n"
-fi
+#fi
 
 # install seashell
 if [ ! -d seashell ]; then
-    git clone https://github.com/ve5li/seashell &&
+    sudo git clone https://github.com/ve5li/seashell &&
+    sudo chown -R "$USER" seashell/
     echo -e "\nseashell installed successfully\n"
 fi
 
 # install shego
 if [ ! -d shego ]; then
-    git clone https://github.com/ve5li/shego &&
+    sudo git clone https://github.com/ve5li/shego &&
+    sudo chown -R "$USER" shego/
     echo -e "\nshego installed successfully\n"
+fi
+
+# install liquify
+if [ ! -d liquify ]; then
+    sudo git clone https://github.com/ve5li/liquify &&
+    sudo chown -R "$USER" liquify/
+    sudo cp "$script_path/liquify" /usr/bin/liquify &&
+    sudo chmod +x /usr/bin/liquify &&
+    echo -e "\nliquify installed successfully\n"
 fi
 
 # install cipher
 if [ ! -d cipher ]; then
-    git clone https://github.com/ve5li/cipher &&
+    sudo git clone https://github.com/ve5li/cipher &&
+    sudo chown -R "$USER" cipher/
     sudo cp "$script_path/cipher" /usr/bin/cipher &&
     sudo chmod +x /usr/bin/cipher &&
     echo -e "\ncipher installed successfully\n"
@@ -39,7 +44,8 @@ fi
 
 # install doofenshmirtz
 if [ ! -d doofenshmirtz ]; then
-    git clone https://github.com/ve5li/doofenshmirtz &&
+    sudo git clone https://github.com/ve5li/doofenshmirtz &&
+    sudo chown -R "$USER" doofenshmirtz/
     sudo cp "$script_path/doofenshmirtz" /usr/bin/doofenshmirtz &&
     sudo chmod +x /usr/bin/doofenshmirtz &&
     echo -e "\ndoofenshmirtz installed successfully\n"
@@ -47,7 +53,8 @@ fi
 
 # install entleman
 if [ ! -d entleman ]; then
-    git clone https://github.com/ve5li/entleman &&
+    sudo git clone https://github.com/ve5li/entleman &&
+    sudo chown -R "$USER" entleman/
     sudo cp "$script_path/entleman" /usr/bin/entleman &&
     sudo chmod +x /usr/bin/entleman &&
     echo -e "\nentleman installed successfully\n"
--- utility/uninstall.sh
+++ utility/uninstall.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 # delete tortoise
-sudo rm -rf /usr/share/tortoise
+sudo rm -rf /usr/share/seamonkey
+sudo rm -rf /usr/share/seashell
+sudo rm -rf /usr/share/shego
+sudo rm -rf /usr/share/liquify
+sudo rm -rf /usr/share/cipher
+sudo rm -rf /usr/share/doofenshmirtz
+sudo rm -rf /usr/share/entleman
 
 # remove start scripts
 sudo rm /usr/bin/seashell
+sudo rm /usr/bin/liquify
 sudo rm /usr/bin/cipher
 sudo rm /usr/bin/doofenshmirtz
 sudo rm /usr/bin/entleman
